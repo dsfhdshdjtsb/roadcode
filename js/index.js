428641abe3d9c1f18806f131bb1cfe093b92e798
kYpranite <39254153+kYpranite@users.noreply.github.com>
2022-06-24 14:21:13 -0700

    added basic barebones map and api key

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-api_key = "5ae2e3f221c38a28845f05b63f732523be8926b6484b88151a8dd476";
+otmApiKey= "5ae2e3f221c38a28845f05b63f732523be8926b6484b88151a8dd476";
+gmApiKey = ""
 
 
 
@@ -9,6 +10,15 @@ const userAction = async (link) => {
     // do something with myJson
   }
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 36.967243, lng: -99.771556},
+        zoom: 5,
+    });
+}
 
-  userAction('http://api.opentripmap.com/0.1/en/places/bbox?lon_min=38.364285&lat_min=59.855685&lon_max=38.372809&lat_max=59.859052&kinds=churches&format=geojson&apikey=' + api_key);
-  userAction('http://api.opentripmap.com/0.1/en/places/xid/W37900074?apikey=' + api_key);
+  
+
+
+  userAction('http://api.opentripmap.com/0.1/en/places/bbox?lon_min=38.364285&lat_min=59.855685&lon_max=38.372809&lat_max=59.859052&kinds=churches&format=geojson&apikey=' + otmApiKey);
+  userAction('http://api.opentripmap.com/0.1/en/places/xid/W37900074?apikey=' + otmApiKey);
