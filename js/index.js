972d0f351feb84e6c7fe22ea94c6317a54a218ee
kYpranite <39254153+kYpranite@users.noreply.github.com>
2022-06-24 16:03:20 -0700

    done

--- js/index.js
+++ js/index.js
@@ -1,21 +1,40 @@
 otmApiKey= "5ae2e3f221c38a28845f05b63f732523be8926b6484b88151a8dd476";
-gmApiKey = ""
+gmApiKey = "AIzaSyCTlMIrLbEtYu8K7Kheto9hxaIqWjzOQ8E"
 
-
-
-const userAction = async (link) => {
+const fetchJson = async (link) => {
     const response = await fetch(link);
     const myJson = await response.json(); //extract JSON from the http response
-    console.log(myJson);
-    // do something with myJson
-  }
+}
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 36.967243, lng: -99.771556},
+    const directionsRenderer = new google.maps.DirectionsRenderer({draggable: true});
+    const directionsService = new google.maps.DirectionsService();
+    map = new google.maps.Map($("#map")[0], {
+        center: { lat: 36.967243, lng: -99.771556}, //center of US
         zoom: 5,
     });
+    directionsRenderer.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+};
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  const waypoints = [{location: "bakersfield, ca"}, {location:"tampa, fl"}]; //order doesn't matter as long as optimizeWaypoints is set to true
+
+  directionsService
+    .route({
+      origin: "Irvine, CA", //can also take placeId and long/lat
+      destination: "New York City, New York",
+      waypoints: waypoints,
+      travelMode: "DRIVING",
+      optimizeWaypoints: true,
+    })
+    .then((response) => {
+      directionsRenderer.setDirections(response); //if direction service receives a response, then render the directions given
+    })
+    .catch((e) => window.alert("Directions request failed")); //else no response, leave error message
 }
+
+
 
   
 
