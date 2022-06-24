a89ce469d69599763b298ab9e8bef5f0eeab89d8
kYpranite <39254153+kYpranite@users.noreply.github.com>
2022-06-24 11:48:29 -0700

    poop

--- js/index.js
+++ js/index.js
@@ -2,12 +2,13 @@ api_key = "5ae2e3f221c38a28845f05b63f732523be8926b6484b88151a8dd476";
 
 
 
-const userAction = async () => {
-    const response = await fetch('http://api.opentripmap.com/0.1/en/places/xid/Q372040?apikey=5ae2e3f221c38a28845f05b63f732523be8926b6484b88151a8dd476');
+const userAction = async (link) => {
+    const response = await fetch(link);
     const myJson = await response.json(); //extract JSON from the http response
     console.log(myJson);
     // do something with myJson
   }
 
 
-  userAction();
+  userAction('http://api.opentripmap.com/0.1/en/places/bbox?lon_min=38.364285&lat_min=59.855685&lon_max=38.372809&lat_max=59.859052&kinds=churches&format=geojson&apikey=' + api_key);
+  userAction('http://api.opentripmap.com/0.1/en/places/xid/W37900074?apikey=' + api_key);
