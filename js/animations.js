7ac8ecb544721ef2dadc6503a4ea64b0197ef023
kYpranite <39254153+kYpranite@users.noreply.github.com>
2022-06-24 22:21:55 -0700

    added a working navbar with animation!!!!!!

--- js/animations.js
+++ js/animations.js
@@ -1,2 +1,11 @@
 
-$(".navbar").animate({width:'toggle'},250);
+// $(".container").animate({width:'5%'}, 250);
+// $(".createBtn").animate({width:'toggle%'}, 250);
+
+$(".menuIcon").click(()=>{
+    if ($(".container").css("left") == "-302px") {
+        $(".container").animate({left: "0px"}, 350);
+    }else{
+        $(".container").animate({left: "-302px"}, 350);
+    }
+});
