98f87c8bb7eab7505d5bdb5c27faa1588270500c
Nicholas Suh <nicksuh17@gmail.com>
2022-06-24 17:54:15 -0700

    cool methods

--- js/index.js
+++ js/index.js
@@ -7,8 +7,6 @@ const fetchJson = async (link) => {
     const myJson = await response.json(); //extract JSON from the http response
 }
 
-
-
 const userAction = async (link) => {
     const response = await fetch(link);
     return await response.json(); //extract JSON from the http response
@@ -47,6 +45,7 @@ async function genDestinationPoint(lat, lon, radius)
     }
     return false;
 }
+
 function generateRandomPoint(lat, lon, radius) {
     var x0 = lon;
     var y0 = lat;
@@ -74,6 +73,36 @@ function toString(kinds){
         result += kinds[i]
     }
     return result
+}
+
+function genWaypoints(waypointList, num, kinds){
+    let locationList = [];
+    for(let i = 0; i < 4; i++)
+    {
+        let thisPoint = Math.floor(Math.random() * (waypointList.length / 5 * (i+1) - waypointList.length / 5 * i) + waypointList.length / 5 * i);
+        let attractions = getAttractions(16093, thisPoint.lat(), thispoint.lng(), toString(kinds));
+        for(let j = 0; j < num/5; j++)
+        {
+            locationList.push(attractions[Math.floor(Math.random() * (attractions.length  + 1))])
+        }
+    }
+    return locationList;
+}
+
+function genReturnWaypoint(lat_1, lon_1, lat_2, lon_2)
+{
+    if(!getWater(lat_1, lon_2).water)
+    {
+        return {"lat": lat_1, "lon": lon_2}
+    }
+    else if (!getWater(lat_2, lon_1).water)
+    {
+        return {"lat": lat_2, "lon": lon_1}
+    }
+    else
+    {
+        return false
+    }
 }
 async function logPromiseResult() {
     console.log(await getAttractions(1000000, 33.656956, -117.798633, ["historic"]));
@@ -111,12 +140,9 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     .catch((e) => window.alert("Directions request failed")); //else no response, leave error message
 }
 
-
-
-  
-
-
 userAction('http://api.opentripmap.com/0.1/en/places/bbox?lon_min=38.364285&lat_min=59.855685&lon_max=38.372809&lat_max=59.859052&kinds=churches&format=geojson&apikey=' + otmApiKey);
 userAction('http://api.opentripmap.com/0.1/en/places/xid/W37900074?apikey=' + otmApiKey);
 genDestinationPoint(33.656956, -117.798633, 100000)
 // console.log(generateRandomPoint( 33.656956, -117.798633, 100000));
+
+console.log(genWaypoints(0, 0, 25, 25, 10));
