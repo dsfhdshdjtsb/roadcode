ae820fe1cbed643c4cf1d49dbfee5db5b1e3fc8b
kYpranite <39254153+kYpranite@users.noreply.github.com>
2022-06-25 02:03:35 -0700

    added animated collapsing checkboxes

--- js/animations.js
+++ js/animations.js
@@ -3,9 +3,19 @@
 // $(".createBtn").animate({width:'toggle%'}, 250);
 
 $(".menuIcon").click(()=>{
-    if ($(".container").css("left") == "-302px") {
+    if ($(".container").css("left") == "-305px") {
         $(".container").animate({left: "0px"}, 350);
     }else{
-        $(".container").animate({left: "-302px"}, 350);
+        $(".container").animate({left: "-305px"}, 350);
     }
 });
+
+$(".titleCheck").on("click", function(e){
+    
+    $(this).next().children().slideToggle();
+
+    // console.log("Added!");
+    // let clicked = $(this.element);
+    // let category = clicked.siblings();
+    // category.fadeOut();
+})
